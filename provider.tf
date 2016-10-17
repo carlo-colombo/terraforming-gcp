b24f0564381aa9201912ed31e74ce6505ce00bb2
Carlo Colombo <ccolombo@pivotal.io>
2016-10-17 15:37:44 +0100

    Reading json and ssl keys from file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = "${var.project}"
   region      = "${var.region}"
-  credentials = "${var.service_account_key}"
+  credentials = "${file(var.service_account_key)}"
 }
